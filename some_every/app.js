4f275ddaf72272422b82ddf76844935108d05f0d
Marina Sukhova <Marna.Sukhova@nytimes.com>
2024-03-18 10:38:03 -0400

    somw fixes

--- some_every/app.js
+++ some_every/app.js
@@ -59,29 +59,3 @@ function hasCertainValue(arr, key, val) {
     return el[key] == val;
   });
 }
-
-const removeRandom = (items) => {
-  let idx = Math.floor(Math.random() * items.length);
-  return [...items.slice(0, idx), ...items.slice(idx + 1)];
-};
-const extend = (array1, array2) => {
-  return [...array1, ...array2];
-};
-const addKeyVal = (obj, key, val) => {
-  let newObj = { ...obj };
-  newObj[key] = val;
-  return newObj;
-};
-const removeKey = (obj, key) => {
-  let newObj = { ...obj };
-  delete newObj[key];
-  return newObj;
-};
-const combine = (obj1, obj2) => {
-  return { ...obj1, ...obj2 };
-}
-const update = (obj, key, val) => {
-  let nweObj = {...obj};
-  newObj[key] = val;
-  return newObj;
-}
