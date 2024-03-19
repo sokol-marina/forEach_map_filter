2dce5041a1185cd1f199ba39fa0f38e063bb9c02
Marina Sukhova <Marna.Sukhova@nytimes.com>
2024-03-18 21:08:55 -0400

    adding the destructuring folder

---
+++ destructuring/app.js
@@ -0,0 +1,70 @@
+
+// Output: 8
+// let facts = {numPlanets: 8, yearNeptuneDiscovered: 1846};
+// let {numPlanets, yearNeptuneDiscovered} = facts;
+// console.log(numPlanets);
+// console.log(yearNeptuneDiscovered);
+// Output: 1846
+
+// let planetFacts = {
+//     numPlanets: 8,
+//     yearNeptuneDiscovered: 1846,
+//     yearMarsDiscovered: 1659
+//   };
+// let {numPlanets, ...discoveryYears} = planetFacts;
+// Output: {yearNeptuneDiscovered: 1846, yearMarsDiscovered: 1659}
+
+
+function getUserData({firstName, favoriteColor="green"}){
+    return `Your name is ${firstName} and you like ${favoriteColor}`;
+  }
+  
+  getUserData({firstName: "Alejandro", favoriteColor: "purple"}) 
+  //'Your name is Alejandro and you like purple'
+  getUserData({firstName: "Melissa"}) 
+  // 'Your name is Melissa and you like green'
+  getUserData({}) 
+  // Your name is undefined and you like green'
+
+let [first, second, third] = ["Maya", "Marisa", "Chi"];
+
+console.log(first); // 'Maya'
+console.log(second); // 'Marisa'
+console.log(third); // 'Chi'
+
+let [raindrops, whiskers, ...aFewOfMyFavoriteThings] = [
+    "Raindrops on roses",
+    "whiskers on kittens",
+    "Bright copper kettles",
+    "warm woolen mittens",
+    "Brown paper packages tied up with strings"
+  ]
+  
+  console.log(raindrops); //     "Raindrops on roses"
+  console.log(whiskers); // "whiskers on kittens"
+  console.log(aFewOfMyFavoriteThings); //  ['Bright copper kettles', 'warm woolen mittens', 'Brown paper packages tied up with strings']
+
+ let numbers = [10, 20, 30];
+[numbers[1], numbers[2]] = [numbers[2], numbers[1]]
+
+console.log(numbers) // [10, 30, 20]
+
+// ES5 Assigning Variables to Object Properties
+var obj = {
+  numbers: {
+    a: 1,
+    b: 2
+  }
+};
+
+const {a,b } = obj.numbers
+
+//ES5 Array Swap
+var arr = [1, 2];
+
+[arr[0], arr[1]] = [arr[1], arr[0]]
+
+// raceResults()
+
+const raceResults = ([first, second, third, ...rest])=>({
+     first, second, third, rest })
